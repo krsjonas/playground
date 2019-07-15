273caa780899d956bd86ea9db63eca9ece094385
Jonas Kraus <bigbossi16@yahoo.de>
2019-07-15 14:18:57 +0200

    Update

--- simple questions_en.py
+++ simple questions_en.py
@@ -1,5 +1,7 @@
 # -*- coding: utf-8 -*-
 
+# locale: en-US
+
 import time
 
 print("In advance: This script is fully GDPR/DSGVO compliant and does not store any input data! ;-)")
@@ -50,7 +52,7 @@ print("-------------------------------------------------------------------")
 print("A little calculator")
 print("Disclaimer: This calculator only does +, -, * and / operations.")
 
-print("How many numbers to operate with? (1-15)")
+print("How many numbers to operate with? (1-17)")
 num_of_numbers = int(input())
 
 if num_of_numbers == 1:
@@ -439,6 +441,77 @@ elif num_of_numbers == 15:
         result = num1 * num2 * num3 * num4 * num5 * num6 * num7 * num8 * num9 * num10 * num11 * num12 * num13 * num14 * num15
     elif operator == 4:
         result = num1 / num2 / num3 / num4 / num5 / num6 / num7 / num8 / num9 / num10 / num11 / num12 / num13 / num14 / num15
+    else:
+        print("Invalid input for an operator.")
+    print("The result is " + str(result) + ".")
+    
+elif num_of_numbers == 16:
+    num1 = int(input("Enter the first number: "))
+    num2 = int(input("Enter the second number: "))
+    num3 = int(input("Enter the third number: "))
+    num4 = int(input("Enter the fourth number: "))
+    num5 = int(input("Enter the fifth number: "))
+    num6 = int(input("Enter the sixth number: "))
+    num7 = int(input("Enter the seventh number: "))
+    num8 = int(input("Enter the eighth number: "))
+    num9 = int(input("Enter the nineth number: "))
+    num10 = int(input("Enter the tenth number: "))
+    num11 = int(input("Enter the eleventh number: "))
+    num12 = int(input("Enter the twelveth number: "))
+    num13 = int(input("Enter the thirteenth number: "))
+    num14 = int(input("Enter the fourteenth number: "))
+    num15 = int(input("Enter the fifteenth number: "))
+    num16 = int(input("Enter the sixteenth number: "))
+    print("Which operator do you want to use?")
+    print("1: +")
+    print("2: -")
+    print("3: *")
+    print("4: /")
+    operator = int(input())
+    if operator == 1:
+        result = num1 + num2 + num3 + num4 + num5 + num6 + num7 + num8 + num9 + num10 + num11 + num12 + num13 + num14 + num15 + num16
+    elif operator == 2:
+        result = num1 - num2 - num3 - num4 - num5 - num6 - num7 - num8 - num9 - num10 - num11 - num12 - num13 - num14 - num15 - num16
+    elif operator == 3:
+        result = num1 * num2 * num3 * num4 * num5 * num6 * num7 * num8 * num9 * num10 * num11 * num12 * num13 * num14 * num15 * num16
+    elif operator == 4:
+        result = num1 / num2 / num3 / num4 / num5 / num6 / num7 / num8 / num9 / num10 / num11 / num12 / num13 / num14 / num15 / num16
+    else:
+        print("Invalid input for an operator.")
+    print("The result is " + str(result) + ".")
+
+elif num_of_numbers == 17:
+    num1 = int(input("Enter the first number: "))
+    num2 = int(input("Enter the second number: "))
+    num3 = int(input("Enter the third number: "))
+    num4 = int(input("Enter the fourth number: "))
+    num5 = int(input("Enter the fifth number: "))
+    num6 = int(input("Enter the sixth number: "))
+    num7 = int(input("Enter the seventh number: "))
+    num8 = int(input("Enter the eighth number: "))
+    num9 = int(input("Enter the nineth number: "))
+    num10 = int(input("Enter the tenth number: "))
+    num11 = int(input("Enter the eleventh number: "))
+    num12 = int(input("Enter the twelveth number: "))
+    num13 = int(input("Enter the thirteenth number: "))
+    num14 = int(input("Enter the fourteenth number: "))
+    num15 = int(input("Enter the fifteenth number: "))
+    num16 = int(input("Enter the sixteenth number: "))
+    num17 = int(input("Enter the seventeenth number: "))
+    print("Which operator do you want to use?")
+    print("1: +")
+    print("2: -")
+    print("3: *")
+    print("4: /")
+    operator = int(input())
+    if operator == 1:
+        result = num1 + num2 + num3 + num4 + num5 + num6 + num7 + num8 + num9 + num10 + num11 + num12 + num13 + num14 + num15 + num16 + num17
+    elif operator == 2:
+        result = num1 - num2 - num3 - num4 - num5 - num6 - num7 - num8 - num9 - num10 - num11 - num12 - num13 - num14 - num15 - num16 - num17
+    elif operator == 3:
+        result = num1 * num2 * num3 * num4 * num5 * num6 * num7 * num8 * num9 * num10 * num11 * num12 * num13 * num14 * num15 * num16 * num17
+    elif operator == 4:
+        result = num1 / num2 / num3 / num4 / num5 / num6 / num7 / num8 / num9 / num10 / num11 / num12 / num13 / num14 / num15 / num16 / num17
     else:
         print("Invalid input for an operator.")
     print("The result is " + str(result) + ".")
