82dbaa44a257b84aed24d942bad032370b5fe528
Jonas <bigbossi16@yahoo.de>
2019-08-17 00:01:38 +0200

    Update

--- simple questions_de.py
+++ simple questions_de.py
@@ -256,6 +256,35 @@ elif number_of_numbers == 9:
         print("Ungültige Eingabe für einen Operator.")
     print("Das Ergebnis ist " + str(result) + ".")
 
+elif number_of_numbers == 10:
+    number_1 = int(input("Gib die erste Zahl ein: "))
+    number_2 = int(input("Gib die zweite Zahl ein: "))
+    number_3 = int(input("Gib die dritte Zahl ein: "))
+    number_4 = int(input("Gib die vierte Zahl ein: "))
+    number_5 = int(input("Gib die fünfte Zahl ein: "))
+    number_6 = int(input("Gib die sechste Zahl ein: "))
+    number_7 = int(input("Gib die siebte Zahl ein: "))
+    number_8 = int(input("Gib die achte Zahl ein: "))
+    number_9 = int(input("Gib die neunte Zahl ein: "))
+    number_10 = int(input("Gib die zehnte Zahl ein: "))
+    print("Welchen Operator willst Du benutzen?")
+    print("1: +")
+    print("2: -")
+    print("3: *")
+    print("4: /")
+    operator = int(input())
+    if operator == 1:
+        result = number_1 + number_2 + number_3 + number_4 + number_5 + number_6 + number_7 + number_8 + number_9 + number_10
+    elif operator == 2:
+        result = number_1 - number_2 - number_3 - number_4 - number_5 - number_6 - number_7 - number_8 - number_9 - number_10
+    elif operator == 3:
+        result = number_1 * number_2 * number_3 * number_4 * number_5 * number_6 * number_7 * number_8 * number_9 * number_10
+    elif operator == 4:
+        result = number_1 / number_2 / number_3 / number_4 / number_5 / number_6 / number_7 / number_8 / number_9 / number_10
+    else:
+        print("Ungültige Eingabe für einen Operator.")
+    print("Das Ergebnis ist " + str(result) + ".")
+
 else:
     print("Zu viele oder zu wenige Zahlen oder fehlerhafte Eingabe.")
 
