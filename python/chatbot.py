95e84a8d33a27c6595e3bed5d92ab1de9f9b3189
Jonas Kraus <bigbossi16@yahoo.de>
2020-01-22 09:12:00 +0100

    Created chatbot.py

---
+++ python/chatbot.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+
+print("Hallo! Ich bin Chatty! Wie heißt Du?")
+name = str(input("Ich heiße: "))
+print("Hallo, " + name + "!")
+
+print("Kleiner Hinweis: Wenn Du nicht mehr mit mir chatten möchtest, drücke einfach Strg+Z")  # Tastenkombination überprüfen und implementieren!
+
+i = 0
+while i == 0:
+    print("Gib hier eine Frage ein:")
+    answer = str(input())
+    if answer == "Wie heißt Du?":
+        print("Ich bin Chatty!")
+    elif answer == "Wie heißt du?":
+        print("Ich bin Chatty!")
+    elif answer == "Wie alt bist Du?":
+        print("Ich wurde im Januar 2020 programmiert, also bin ich noch nicht einmal ein Jahr alt.")
+    elif answer == "Wie alt bist du?":
+        print("Ich wurde im Januar 2020 programmiert, also bin ich noch nicht einmal ein Jahr alt.")
+    elif answer == "Was hast Du an?":
+        print("Ich als Bot bin nicht real, darum habe ich auch keine Kleidung.")
+    elif answer == "Was hast du an?":
+        print("Ich als Bot bin nicht real, darum habe ich auch keine Kleidung.")
+    else:
+        print("Tut mir leid, ich habe Deine Frage nicht verstanden.")
