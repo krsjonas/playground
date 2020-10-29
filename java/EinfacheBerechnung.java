24c47a7439a088c091cffc10c2bd0e49f79837a4
krsjonas <51300199+krsjonas@users.noreply.github.com>
2020-10-30 00:11:42 +0100

    Update

--- java/EinfacheBerechnung.java
+++ java/EinfacheBerechnung.java
@@ -4,33 +4,29 @@ import java.util.Scanner;
 
 public class EinfacheBerechnung {
 	public static void main (String[] args) {
-		System.out.println("Gib die erste Zahl ein: ");
-		Scanner one = new Scanner(System.in);
-		int zahl1 = one.nextInt();
+		Scanner nums = new Scanner(System.in);
 		
-		System.out.println("Gib den Operator (+, -, *, /) ein: ");
+		System.out.print("Gib die erste Zahl ein: ");
+		int zahl1 = nums.nextInt();
+		
+		System.out.print("Gib den Operator (+, -, *, /) ein: ");
 		Scanner op = new Scanner(System.in);
 		String operator = op.nextLine();
 		
 		// System.out.println(operator);
 		
-		System.out.println("Gib die zweite Zahl ein: ");
-		Scanner two = new Scanner(System.in);
-		int zahl2 = two.nextInt();
+		System.out.print("Gib die zweite Zahl ein: ");
+		int zahl2 = nums.nextInt();
 		
 		int ergebnis = 0;
-		// String plus = "+";
-		// String minus = "-";
-		// String multiply = "*";
-		// String divide = "/";
 		
-		if (operator == "+") {
+		if (operator.equals("+")) {
 			ergebnis = zahl1 + zahl2;
-		} else if (operator == "-") {
+		} else if (operator.equals("-")) {
 			ergebnis = zahl1 - zahl2;
-		} else if (operator == "*") {
+		} else if (operator.equals("*")) {
 			ergebnis = zahl1 * zahl2;
-		} else if (operator == "/") {
+		} else if (operator.equals("/")) {
 			ergebnis = zahl1 / zahl2;
 		} else {
 			System.out.println("Es wurde kein gültiger Operator eingegeben.");
